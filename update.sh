e8de4853e2485cfe7878a06e80d63a8354de473c
Toai Vo <toaivo@stanford.edu>
2023-04-04 16:22:45 -0700

    Fix typo

--- update.sh
+++ update.sh
@@ -2,11 +2,9 @@
 #  this script generates new access.db, sendmail.cf and authinfo.db
 #  See document https://docs.aws.amazon.com/ses/latest/dg/send-email-sendmail.html
 
-
-makemap hash /etc/mail/authinfo.db < /etc/mail/authinfo
-makemap hash /etc/mail/access.db < /etc/mail/access
-m4 /etc/mail/sendmail.mc > /etc/mail/sendmail.cf
-chmod 640 /etc/mail/authinfo
+makemap hash /etc/mail/authinfo.db < /tmp/ses-mailx/authinfo
+makemap hash /etc/mail/access.db < /tmp/ses-mailx/access
+m4 /tmp/ses-mailx/sendmail.mc > /etc/mail/sendmail.cf
 
 systemctl stop sendmail
 systemctl start sendmail
